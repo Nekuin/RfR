0537e38fad1e5471688514c7719428be7547f69a
Nekuin <Nekuin@gmail.com>
2018-09-29 19:23:03 +0300

    project + .gitignore
    all dat good stuff

---
+++ RfR/Source/RfR/RfRGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RfRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RFR_API ARfRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
